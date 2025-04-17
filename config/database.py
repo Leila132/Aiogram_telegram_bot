659637ef9b38d56f16b1589796937029e7b95a01
Leila Saryimova <saryymova@mail.ru>
2025-04-17 14:40:12 +0300

    extra imports have been removed

--- config/database.py
+++ config/database.py
@@ -1,8 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.asyncio import AsyncSession
 
 DATABASE_URL = "sqlite+aiosqlite:///./my_database.db"
 
