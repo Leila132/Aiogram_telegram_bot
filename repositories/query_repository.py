d8b9cf72e46fa50ab772d72104a80acb95e45c99
Leila Saryimova <saryymova@mail.ru>
2025-04-17 14:54:58 +0300

    use AsyncSession

--- repositories/query_repository.py
+++ repositories/query_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from models import Query
 from typing import List
 from sqlalchemy.exc import SQLAlchemyError
@@ -7,7 +7,7 @@ from sqlalchemy import select
 
 
 class QueryRepository:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
         self.db = db
 
     async def create(self, query_data: dict) -> Query:
